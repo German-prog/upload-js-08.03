21fcd5ea0cec479e2be0119f278c9b7324cf7a1d
German-prog <56883343+German-prog@users.noreply.github.com>
2020-03-08 13:35:35 +0200

    Add files via upload

---
+++ scripts/index.js
@@ -0,0 +1,83 @@
+//Number("12");
+//"String";
+//console.log(Number("12"));
+
+//Number.parseInt("12px");
+//Number.parseFloat("12.345px");
+//console.log(Number.parseInt("12px"));
+//console.log(Number.parseFloat("12.345px"));
+//console.log(Number.parseInt("12.345px"));
+//console.log(Number.parseFloat("12px"));
+//console.log(parseInt("20",2));
+
+//const number= Number("24");
+//console.log(Number.isNaN(24));
+
+//const isEqual =0.1+0.2==0.3;
+//console.log(isEqual);
+
+//const isRightEqual =(0.1*10+0.2*10)/10==(0.3*10)/10 ;
+//console.log(isRightEqual);
+
+//Math.pow(2.16);
+//Math.floor(1.7);
+//Math.ceil(1.2);
+//Math.round(1.7);
+//Math.max(10,20,30,5,8,50);
+//Math.min(10,20,30,5,8,50);
+//console.log(Math.pow(2,16));
+//console.log(Math.floor(1.7));
+//console.log(Math.ceil(1.2));
+//console.log(Math.round(1.7));
+//console.log(Math.max(10,20,30,5,8,50));
+//console.log(Math.min(10,20,30,5,8,50));
+
+//Math.random();
+//console.log(Math.random()*(10-1) +1);
+//console.log(Math.random()*-9);
+
+//console.log("Javascript"[0]);
+//console.log("Javascript"[4]);
+//console.log("Javascript"[8]);
+
+//console.log(5 + 5 +"5");
+
+//const isEqual = true;
+//console.log(isEqual+"false");
+
+//const str = "La la la la";
+//console.log (str.length);
+//console.log(str.toUpperCase());
+//console.log(str.toLowerCase());
+
+//const str ="Hello,SAMP";
+//console.log(str.indexOf("SAMP"));
+
+//const str="Hello my dear CSER Alexandr";
+//console.log(str.includes("Hello my dear"));
+
+//const AMOUNT = 1000000;
+//const str1 = "text" + AMOUNT + "text";
+//const str2 = `text ${AMOUNT} text`;
+//console.log(str1);
+//console.log(str2);
+
+//console.log(Boolean(0));
+//console.log(Boolean(""));
+//console.log(Boolean(false));
+//console.log(Boolean(null));
+//console.log(Boolean(undefined));
+//console.log(Boolean(NaN));
+
+// const num = 8;
+// const res=num>5&& num<100;
+// console.log(res);
+
+//const isForHome=apple.size=="small";
+//apple.color=="green" ||apple.color=="green";
+
+// const day=9;
+// const month="March";
+// const isTodayWomenDay = day==8 &&month=="March";
+// if(!isTodayWomenDay);
+
